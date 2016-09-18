74b819608d466f0de4a6ac44b26ac8a17b4c442b
阳永辉 <hopeyyh@qq.com>
2016-09-18 17:39:37 +0800

    Initial Commit

---
+++ UITextFieldAttributeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITextFieldAttributeDemo
+//
+//  Created by 阳永辉 on 16/9/18.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITextFieldAttributeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITextFieldAttributeDemo
+//
+//  Created by 阳永辉 on 16/9/18.
+//  Copyright © 2016年 netposa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
